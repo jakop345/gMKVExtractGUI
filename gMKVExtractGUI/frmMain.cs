7b969dba4529ee306c96b0cd34f256e3e38dbecc
gpower2 <gpower2@48fa41d4-84fb-42fd-86e6-1c8696aa3653>
2013-11-23 13:14:08 +0000

    Initial commit
    git-svn-id: svn://svn.code.sf.net/p/gmkvextractgui/code@1 48fa41d4-84fb-42fd-86e6-1c8696aa3653

---
+++ gMKVExtractGUI/frmMain.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+
+namespace gMKVExtractGUI
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            Text = "gMKVExtractGUI v" + Assembly.GetExecutingAssembly().GetName().Version + " -- By Gpower2";
+            gMKVExtract g = new gMKVExtract();
+            Debug.WriteLine(g.GetMKVToolnixPath());
+            g.GetMKVTracks("");
+        }
+    }
+}

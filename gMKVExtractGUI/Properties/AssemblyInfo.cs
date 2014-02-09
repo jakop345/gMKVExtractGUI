2e7cbc2b9ef7cb12be4d4c7870f80211e93f6f3d
gpower2 <gpower2@48fa41d4-84fb-42fd-86e6-1c8696aa3653>
2014-02-09 21:02:33 +0000

    - Updated Version to 1.1 - Updated Assembly Information
    git-svn-id: svn://svn.code.sf.net/p/gmkvextractgui/code@6 48fa41d4-84fb-42fd-86e6-1c8696aa3653

--- gMKVExtractGUI/Properties/AssemblyInfo.cs
+++ gMKVExtractGUI/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("gMKVExtractGUI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A GUI for mkvextract utility (part of MKVToolnix)")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Gpower2")]
 [assembly: AssemblyProduct("gMKVExtractGUI")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]

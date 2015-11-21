3a620a030f53f02766ab3f725e6b84d9baef9f57
gpower2 <gpower2@48fa41d4-84fb-42fd-86e6-1c8696aa3653>
2015-11-21 10:22:34 +0000

    - Add a start up check to ensure that gMKVToolnix.dll is present in the same directory as gMKVExtractGUI.exe - Rename IsLinux property to IsOnLinux to make more sense
    git-svn-id: svn://svn.code.sf.net/p/gmkvextractgui/code@83 48fa41d4-84fb-42fd-86e6-1c8696aa3653

--- gMKVExtractGUI/Program.cs
+++ gMKVExtractGUI/Program.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
+using System.IO;
 
 namespace gMKVToolnix
 {
     static class Program
     {
+        /// <summary>
+        /// Returns if the running Platform is Linux Or MacOSX
+        /// </summary>
+        public static Boolean IsOnLinux
+        {
+            get
+            {
+                PlatformID myPlatform = Environment.OSVersion.Platform;
+                // 128 is Mono 1.x specific value for Linux systems, so it's there to provide compatibility
+                return (myPlatform == PlatformID.Unix) || (myPlatform == PlatformID.MacOSX) || ((Int32)myPlatform == 128);
+            }
+        }
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -14,14 +28,21 @@ namespace gMKVToolnix
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            // If on Linux, set the enironmnet variables for locale to C (default locale)
-            if (gMKVHelper.IsLinux)
+            if (!File.Exists(Path.Combine(Application.StartupPath, "gMKVToolnix.dll")))
             {
-                Environment.SetEnvironmentVariable("LC_ALL", "C", EnvironmentVariableTarget.Process);
-                Environment.SetEnvironmentVariable("LANG", "C", EnvironmentVariableTarget.Process);
-                Environment.SetEnvironmentVariable("LC_MESSAGES", "C", EnvironmentVariableTarget.Process);
+                MessageBox.Show("The gMKVToolnix.dll was not found! Please download and reinstall gMKVExtractGUI!", "An error has occured!", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-            Application.Run(new frmMain());
+            else
+            {
+                // If on Linux, set the enironmnet variables for locale to C (default locale)
+                if (IsOnLinux)
+                {
+                    Environment.SetEnvironmentVariable("LC_ALL", "C", EnvironmentVariableTarget.Process);
+                    Environment.SetEnvironmentVariable("LANG", "C", EnvironmentVariableTarget.Process);
+                    Environment.SetEnvironmentVariable("LC_MESSAGES", "C", EnvironmentVariableTarget.Process);
+                }
+                Application.Run(new frmMain());
+            }
         }
     }
 }
